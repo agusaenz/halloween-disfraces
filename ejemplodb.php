7f054e12627ef397de15d45613cbcb08448292b3
agusaenz <157411527+agusaenz@users.noreply.github.com>
2024-05-18 18:32:39 -0300

    incluyo jquery

--- ejemplodb.php
+++ ejemplodb.php
@@ -47,7 +47,9 @@ $db->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ejemplo</title>
+
+    <script src="js/jquery-3.7.1.min.js"></script>
 </head>
 <body>
     <h1>hola</h1>
