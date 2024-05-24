862e9221fec70b320e0e3b1f0898395377d5abc3
ajs1905 <agusantos80@gmail.com>
2024-05-24 15:38:15 -0300

    Add files via upload

---
+++ aut_config.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+// local
+$sql_host = "localhost"; // nombre del servidor
+$sql_usuario = "root"; // usuario
+$sql_pass = ""; // contraseña
+$sql_db = "test"; // nombre de la database
+// $sql_db = "halloween-disfraces";
