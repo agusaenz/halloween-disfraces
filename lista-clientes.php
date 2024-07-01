979a6fb777bb4481817e3e66a06dc006322dbccf
valenloza <valen12loza@gmail.com>
2024-07-01 13:04:19 -0300

    arreglos

--- lista-clientes.php
+++ lista-clientes.php
@@ -44,6 +44,13 @@
     .move-left {
       margin-left: -15px;
     }
+
+    @media (max-width: 768px) {
+      .btn-lg-custom {
+        width: 100%;
+        margin-top: 10px;
+      }
+    }
   </style>
 </head>
 
@@ -52,9 +59,6 @@
     <form class="custom-form" id="resizable-form">
       <h1 class="text-center">Clientes</h1>
       <div class="row">
-        <div class="col-md-3">
-          <input type="text" id="searchName" class="form-control" placeholder="Buscar por nombre o apellido..." />
-        </div>
         <div class="col-md-3">
           <input type="text" id="searchDNI" class="form-control" placeholder="Buscar por DNI..." />
         </div>
@@ -80,7 +84,7 @@
           <table id="client-table" class="table table-bordered">
             <thead>
               <tr>
-                <th>Apellido y Nombre</th>
+                <th>Apellido y Nombres</th>
                 <th>DNI</th>
                 <th>Telefono</th>
                 <th>Domicilio</th>
@@ -96,10 +100,10 @@
 
   <!-- Modal -->
   <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="editModalLabel">Editar Persona</h5>
+          <h5 class="modal-title" id="editModalLabel">Editar Datos</h5>
           <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
         <div class="modal-body">
@@ -109,7 +113,7 @@
               <input type="text" class="form-control" id="editApellido" />
             </div>
             <div class="form-group">
-              <label for="editNombre">Nombre</label>
+              <label for="editNombre">Nombres</label>
               <input type="text" class="form-control" id="editNombre" />
             </div>
             <div class="form-group">
@@ -125,7 +129,7 @@
               <input type="text" class="form-control" id="editTelefono" />
             </div>
             <div class="form-group">
-              <label for="editDireccion">Dirección</label>
+              <label for="editDireccion">Domicilio</label>
               <input type="text" class="form-control" id="editDireccion" />
             </div>
             <input type="hidden" id="editId" />
@@ -142,7 +146,7 @@
 
   <!-- Modal de Agregar -->
   <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="addModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
+    <div class="modal-dialog modal-lg">
       <div class="modal-content">
         <div class="modal-header">
           <h5 class="modal-title" id="addModalLabel">Agregar Cliente</h5>
@@ -155,7 +159,7 @@
               <input type="text" class="form-control" id="addApellido" />
             </div>
             <div class="form-group">
-              <label for="addNombre">Nombre</label>
+              <label for="addNombre">Nombres</label>
               <input type="text" class="form-control" id="addNombre" />
             </div>
             <div class="form-group">
@@ -171,7 +175,7 @@
               <input type="text" class="form-control" id="addTelefono" />
             </div>
             <div class="form-group">
-              <label for="addDireccion">Dirección</label>
+              <label for="addDireccion">Domicilio</label>
               <input type="text" class="form-control" id="addDireccion" />
             </div>
           </form>
@@ -193,7 +197,6 @@
     function generarTablaClientes() {
       if (tabla != undefined) tabla.destroy();
 
-      // let filtroNombre = $('#searchName').val();
       let filtroDNI = $('#searchDNI').val();
 
       let datos = {};
@@ -241,6 +244,7 @@
             width: "22%",
             createdCell: function (td, cellData, rowData, row, col) {
               $(td).addClass('text-center');
+              
             }
           }
         ]
@@ -367,178 +371,14 @@
 
     $(document).ready(function () {
       $('input[type="text"]').val('');
-
       generarTablaClientes();
 
-      // #############################################
-
-      // let people = [
-      //   {
-      //     id: 1,
-      //     apellido: "Pérez",
-      //     nombre: "Juan",
-      //     dni: "12345678",
-      //     email: "juan@example.com",
-      //     telefono: "111-222-333",
-      //     direccion: "Calle 1",
-      //   },
-      //   {
-      //     id: 2,
-      //     apellido: "Gómez",
-      //     nombre: "María",
-      //     dni: "87654321",
-      //     email: "maria@example.com",
-      //     telefono: "444-555-666",
-      //     direccion: "Calle 2",
-      //   },
-      //   {
-      //     id: 3,
-      //     apellido: "Rodríguez",
-      //     nombre: "Luis",
-      //     dni: "11223344",
-      //     email: "luis@example.com",
-      //     telefono: "777-888-999",
-      //     direccion: "Calle 3",
-      //   },
-      //   {
-      //     id: 4,
-      //     apellido: "Martínez",
-      //     nombre: "Ana",
-      //     dni: "55667788",
-      //     email: "ana@example.com",
-      //     telefono: "333-222-111",
-      //     direccion: "Calle 4",
-      //   },
-      //   {
-      //     id: 5,
-      //     apellido: "López",
-      //     nombre: "Carlos",
-      //     dni: "99887766",
-      //     email: "carlos@example.com",
-      //     telefono: "555-444-333",
-      //     direccion: "Calle 5",
-      //   },
-      // ];
-
-      // function renderList(filterName = "", filterDNI = "") {
-      //   const list = $("#person-list");
-      //   list.empty();
-      //   people.forEach((person) => {
-      //     if (
-      //       (person.apellido
-      //         .toLowerCase()
-      //         .includes(filterName.toLowerCase()) ||
-      //         person.nombre
-      //           .toLowerCase()
-      //           .includes(filterName.toLowerCase())) &&
-      //       person.dni.includes(filterDNI)
-      //     ) {
-      //       list.append(`
-      //               <tr>
-      //                   <td>${person.apellido} ${person.nombre}</td>
-      //                   <td>${person.dni}</td>
-      //                   <td>${person.telefono}</td>
-      //                   <td>${person.direccion}</td>
-      //                   <td class="text-center align-middle">
-      //                       <button class="btn btn-primary edit-btn" data-id="${person.id}">Editar</button>
-      //                       <button class="btn btn-danger delete-btn" data-id="${person.id}">Eliminar</button>
-      //                   </td>
-      //               </tr>
-      //           `);
-      //     }
-      //   });
-      // }
-
-      //   function handleSearch() {
-      //     const filterName = $("#searchName").val();
-      //     const filterDNI = $("#searchDNI").val();
-      //     renderList(filterName, filterDNI);
-      //   }
-
-      //   $("#searchName").keypress(function (event) {
-      //     if (event.keyCode === 13) {
-      //       event.preventDefault();
-      //       handleSearch();
-      //     }
-      //   });
-
-      //   $("#searchDNI").keypress(function (event) {
-      //     if (event.keyCode === 13) {
-      //       event.preventDefault();
-      //       handleSearch();
-      //     }
-      //   });
-
-      //   $("#searchBtn").click(function () {
-      //     handleSearch();
-      //   });
-
-      //   $("#clearFiltersBtn").click(function () {
-      //     $("#searchName").val("");
-      //     $("#searchDNI").val("");
-      //     renderList();
-      //   });
-
-      //   $(document).on("click", ".edit-btn", function () {
-      //     event.preventDefault();
-      //     const id = $(this).data("id");
-      //     const person = people.find((p) => p.id == id);
-      //     if (person) {
-      //       $("#editId").val(person.id);
-      //       $("#editApellido").val(person.apellido);
-      //       $("#editNombre").val(person.nombre);
-      //       $("#editDNI").val(person.dni);
-      //       $("#editEmail").val(person.email);
-      //       $("#editTelefono").val(person.telefono);
-      //       $("#editDireccion").val(person.direccion);
-      //       $("#editModal").modal("show");
-      //     }
-      //   });
-
-      //   $("#saveChanges").click(function () {
-      //     event.preventDefault();
-      //     const id = $("#editId").val();
-      //     const person = people.find((p) => p.id == id);
-      //     if (person) {
-      //       person.apellido = $("#editApellido").val();
-      //       person.nombre = $("#editNombre").val();
-      //       person.dni = $("#editDNI").val();
-      //       person.email = $("#editEmail").val();
-      //       person.telefono = $("#editTelefono").val();
-      //       person.direccion = $("#editDireccion").val();
-      //       $("#editModal").modal("hide");
-      //       renderList();
-      //     }
-      //   });
-
-      //   $(document).on("click", ".delete-btn", function () {
-      //     const id = $(this).data("id");
-      //     people = people.filter((p) => p.id !== id);
-      //     renderList();
-      //   });
-
-      //   $("#addClient").click(function () {
-      //     const newPerson = {
-      //       id: people.length ? people[people.length - 1].id + 1 : 1,
-      //       apellido: $("#addApellido").val(),
-      //       nombre: $("#addNombre").val(),
-      //       dni: $("#addDNI").val(),
-      //       email: $("#addEmail").val(),
-      //       telefono: $("#addTelefono").val(),
-      //       direccion: $("#addDireccion").val(),
-      //     };
-      //     people.push(newPerson);
-      //     $("#addModal").modal("hide");
-      //     renderList();
-      //     $("#addForm")[0].reset();
-      //   });
-
-      //   $("#addClientBtn").click(function () {
-      //     $("#addForm")[0].reset();
-      //     $("#addModal").modal("show");
-      //   });
-
-      //   renderList();
+      $('#searchDNI').on('keypress', function (e) {
+        if (e.which == 13) {
+          e.preventDefault();
+          generarTablaClientes();
+        }
+      });
     });
   </script>
 </body>
